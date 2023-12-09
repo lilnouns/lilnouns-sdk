1631e788dc2b15396388b6a1db142681a864034c
Milad Nekofar <milad@nekofar.com>
2023-12-09 21:57:54 +0330

    chore: refactor fetch function in `wagmi.config.ts`
    Improved the fetch function in wagmi.config.ts to use `node-fetch` for HTTP requests. This allows complex requests and error handling to fetch contract data from etherscan. Additionally, function getCacheKey and the custom react and actions plugins have been updated to refine their functionality.

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@wagmi/cli'
 import { actions, fetch, react } from '@wagmi/cli/plugins'
 import changeCase from 'change-case'
+import { default as fetch_ } from 'node-fetch'
 
 function etherscan({ apiKey, name }: { apiKey?: string; name: string }) {
   return fetch({
@@ -22,25 +23,40 @@ function etherscan({ apiKey, name }: { apiKey?: string; name: string }) {
         address: '0x5d2C31ce16924C2a71D317e5BbFd5ce387854039',
       },
     ],
-    getCacheKey({ contract }: { contract: any }) {
-      // Define type for contract
-      return `${changeCase.camelCase(name)}_m:${JSON.stringify(
-        contract.address,
-      )}`
+    getCacheKey({ contract }) {
+      return `${changeCase.camelCase(name)}:${JSON.stringify(contract.address)}`
     },
     async parse({ response }) {
       const json: any = await response.json()
       if (json.status === '0') throw new Error(json.message)
       return JSON.parse(json.result)
     },
-    request(contract: any) {
-      // Define type for contract
+    request(contract) {
       if (!contract.address) throw new Error('address is required')
+
       const address = contract.address
-      const baseUrl = 'https://api.etherscan.io'
-      return {
-        url: `${baseUrl}/api?module=contract&action=getabi&address=${address}&apikey=${apiKey}`,
-      }
+      const baseUrl = 'https://api.etherscan.io/api'
+      const urlQuery = `module=contract&action=getsourcecode&address=${address}&apiKey=${apiKey}`
+
+      return fetch_(`${baseUrl}?${urlQuery.toString()}`)
+        .then((response) => response.json())
+        .then((json: any) => {
+          let urlQuery = `module=contract&action=getabi&address=${address}&apiKey=${apiKey}`
+          if (json.status === '1' && json.message === 'OK') {
+            const { Proxy, Implementation } = json.result[0]
+            const sourceAddress =
+              Proxy === '1' && Implementation ? Implementation : address
+            urlQuery = `module=contract&action=getabi&address=${sourceAddress}&apiKey=${apiKey}`
+          } else {
+            throw new Error('Error fetching contract source')
+          }
+          return {
+            url: `${baseUrl}?${urlQuery.toString()}`,
+          }
+        })
+        .catch((error) => {
+          throw new Error('Server error: ' + error)
+        })
     },
   })
 }
@@ -53,7 +69,22 @@ export default defineConfig({
       name: 'Etherscan',
       apiKey: process.env.ETHERSCAN_API_KEY,
     }),
-    actions(),
-    react(),
+    actions({
+      getContract: true,
+      prepareWriteContract: false,
+      readContract: false,
+      watchContractEvent: false,
+      writeContract: false,
+    }),
+    react({
+      useContractRead: true,
+      useContractWrite: true,
+      useContractEvent: true,
+      useContractFunctionRead: false,
+      useContractFunctionWrite: false,
+      useContractItemEvent: false,
+      usePrepareContractFunctionWrite: false,
+      usePrepareContractWrite: false,
+    }),
   ],
 })
