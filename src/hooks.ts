8a3196caba9134649e274bcd0ee29f46e82c314a
Milad Nekofar <milad@nekofar.com>
2023-12-09 22:44:05 +0330

    feat: add new hooks for auction, governor, token, and treasury
    This commit introduces a new `hooks.ts` file that centralizes the handling of events and actions associated with auctions, governors, tokens, and treasuries. These hooks are exported from a generated file and increase the organization and maintainability of the code.

---
+++ src/hooks.ts
@@ -0,0 +1,14 @@
+export {
+  useAuctionEvent,
+  useAuctionRead,
+  useAuctionWrite,
+  useGovernorEvent,
+  useGovernorRead,
+  useGovernorWrite,
+  useTokenEvent,
+  useTokenRead,
+  useTokenWrite,
+  useTreasuryEvent,
+  useTreasuryRead,
+  useTreasuryWrite,
+} from './generated'
