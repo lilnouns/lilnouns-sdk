01a3721e3aa5d41e3c97d68aae80baaccff817f9
Milad Nekofar <milad@nekofar.com>
2023-12-10 11:35:19 +0330

    chore: refactor and simplify `etherscan` plugin in `wagmi.config.ts`
    The complexity of the 'etherscan' function in 'wagmi.config.ts' has been reduced by moving the contracts out of the function and into the function call. This simplifies the individual method and makes code cleaner. The error handling logic has also been enhanced, making it more readable and debugging friendlier.

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,62 +1,52 @@
-import { defineConfig } from '@wagmi/cli'
+import { ContractConfig, defineConfig } from '@wagmi/cli'
 import { actions, fetch, react } from '@wagmi/cli/plugins'
-import changeCase from 'change-case'
-import { default as fetch_ } from 'node-fetch'
+import { camelCase } from 'change-case'
+import { default as _fetch } from 'node-fetch'
 
-function etherscan({ apiKey, name }: { apiKey?: string; name: string }) {
+function etherscan({
+  apiKey,
+  name,
+  contracts,
+}: {
+  apiKey?: string
+  name: string
+  contracts: Omit<ContractConfig, 'abi'>[]
+}) {
   return fetch({
-    contracts: [
-      {
-        name: 'Token',
-        address: '0x4b10701Bfd7BFEdc47d50562b76b436fbB5BdB3B',
-      },
-      {
-        name: 'Auction',
-        address: '0x55e0F7A3bB39a28Bd7Bcc458e04b3cF00Ad3219E',
-      },
-      {
-        name: 'Treasury',
-        address: '0xd5f279ff9EB21c6D40C8f345a66f2751C4eeA1fB',
-      },
-      {
-        name: 'Governor',
-        address: '0x5d2C31ce16924C2a71D317e5BbFd5ce387854039',
-      },
-    ],
+    contracts: contracts,
     getCacheKey({ contract }) {
-      return `${changeCase.camelCase(name)}:${JSON.stringify(contract.address)}`
+      return `${camelCase(name)}:${JSON.stringify(contract.address)}`
     },
     async parse({ response }) {
-      const json: any = await response.json()
-      if (json.status === '0') throw new Error(json.message)
-      return JSON.parse(json.result)
+      const { message, result, status }: any = await response.json()
+      if (status === '0') throw new Error(message)
+      return JSON.parse(result)
     },
-    request(contract) {
+    async request(contract) {
       if (!contract.address) throw new Error('address is required')
 
       const address = contract.address
       const baseUrl = 'https://api.etherscan.io/api'
       const urlQuery = `module=contract&action=getsourcecode&address=${address}&apiKey=${apiKey}`
 
-      return fetch_(`${baseUrl}?${urlQuery.toString()}`)
-        .then((response) => response.json())
-        .then((json: any) => {
-          let urlQuery = `module=contract&action=getabi&address=${address}&apiKey=${apiKey}`
-          if (json.status === '1' && json.message === 'OK') {
-            const { Proxy, Implementation } = json.result[0]
-            const sourceAddress =
-              Proxy === '1' && Implementation ? Implementation : address
-            urlQuery = `module=contract&action=getabi&address=${sourceAddress}&apiKey=${apiKey}`
-          } else {
-            throw new Error('Error fetching contract source')
-          }
-          return {
-            url: `${baseUrl}?${urlQuery.toString()}`,
-          }
-        })
-        .catch((error) => {
-          throw new Error('Server error: ' + error)
-        })
+      try {
+        const response = await _fetch(`${baseUrl}?${urlQuery.toString()}`)
+        const { message, result, status }: any = await response.json()
+
+        let abiUrlQuery = `module=contract&action=getabi&address=${address}&apiKey=${apiKey}`
+        if (status === '1' && message === 'OK') {
+          const { Proxy, Implementation } = result[0]
+          const sourceAddress =
+            Proxy === '1' && Implementation ? Implementation : address
+          abiUrlQuery = `module=contract&action=getabi&address=${sourceAddress}&apiKey=${apiKey}`
+        }
+
+        return {
+          url: `${baseUrl}?${abiUrlQuery.toString()}`,
+        }
+      } catch (error) {
+        throw new Error('Server error: ' + error)
+      }
     },
   })
 }
@@ -68,6 +58,24 @@ export default defineConfig({
     etherscan({
       name: 'Etherscan',
       apiKey: process.env.ETHERSCAN_API_KEY,
+      contracts: [
+        {
+          name: 'Token',
+          address: '0x4b10701Bfd7BFEdc47d50562b76b436fbB5BdB3B',
+        },
+        {
+          name: 'Auction',
+          address: '0x55e0F7A3bB39a28Bd7Bcc458e04b3cF00Ad3219E',
+        },
+        {
+          name: 'Treasury',
+          address: '0xd5f279ff9EB21c6D40C8f345a66f2751C4eeA1fB',
+        },
+        {
+          name: 'Governor',
+          address: '0x5d2C31ce16924C2a71D317e5BbFd5ce387854039',
+        },
+      ],
     }),
     actions({
       getContract: true,
